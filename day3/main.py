09f1401c080b3840c972f466670335fbb7d1105e
StuartConnell <45386219+StuartConnell@users.noreply.github.com>
2024-12-03 17:42:26 +0000

    finishing day3

--- day3/main.py
+++ day3/main.py
@@ -7,20 +7,52 @@ def get_input() -> str:
     with open("data.txt") as f:
         lines = f.readlines()
 
-    for l in lines:
-        s += l
+    for line in lines:
+        s += line
     return s
+
+
+def filter_mem(mem: str) -> str:
+    do_regex = re.compile(r"do\(\)")
+    dont_regex = re.compile(r"don't\(\)")
+
+    string_to_search = mem
+    while True:
+
+        dont_idxs = [m.start() for m in re.finditer(dont_regex, string_to_search)] 
+        if len(dont_idxs) == 0:
+            break
+
+        dont_idx = dont_idxs[0]
+
+        do_idxs = [m.start() for m in re.finditer(do_regex, string_to_search)] 
+        do_idx = -1
+        for idx in do_idxs:
+            if idx > dont_idx:
+                do_idx = idx
+                break
+
+        if do_idx == -1:
+            string_to_search = string_to_search[:dont_idx]
+            print(string_to_search)
+        else:
+            string_to_search = string_to_search[:dont_idx] + string_to_search[do_idx:]
+            print(string_to_search)
+    return string_to_search
+            
+
 
 def main():
     mem = get_input() 
-    regex = re.compile("mul\([0-9]{1,3},[0-9]{1,3}\)")
+    mem = filter_mem(mem)
 
+    regex = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)")
     idx = [m for m in re.finditer(regex, mem)]
-
+    
     matches = []
     for i in idx:
         matches.append(mem[i.start():i.end()])
-    
+
     results = []
     for m in matches:
         nums = m[4:-1]
